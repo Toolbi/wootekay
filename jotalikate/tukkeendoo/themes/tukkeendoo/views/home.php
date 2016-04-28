3f9e57158b222aae3770c604db15b9b51a14e7b3
llaff <fallfallo@hotmail.fr>
2016-04-29 00:12:53 +0200

    Test login and refactoring

---
+++ jotalikate/tukkeendoo/themes/tukkeendoo/views/home.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Home test</title>
+	</head>
+	<body>
+	
+	<p>Test..</p>
+	
+	<pre>
+		<?php
+		 		if (isset($data)) {
+					 print_r($data) ;
+				 }else{
+				 	echo "There is no data to print !";
+				 }
+		 ?>
+	</pre>
+	
+	</body>
+</html>
