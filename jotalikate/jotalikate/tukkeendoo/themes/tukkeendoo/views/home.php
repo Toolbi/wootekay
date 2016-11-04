1cd52d023e354cdd7a6e642964bc595ad0a88edf
llaff <fallfallo@hotmail.fr>
2016-11-04 22:24:35 +0100

    delete jotalikay/jotalikay

--- jotalikate/jotalikate/tukkeendoo/themes/tukkeendoo/views/home.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php'); ?>
-
-<h3><?php echo "Hello from Home"; ?></h3>
-
-<?php print_r(json_encode($data)); ?>
-         
-<?php include('footer.php'); ?>
--- jotalikate/jotalikate/tukkeendoo/themes/tukkeendoo/views/login.php
+++
@@ -1,6 +0,0 @@
-<?php include('header.php');?>
-
-<h3><?php echo "Hello from Login"; ?></h3>
-		
-
-<?php include('footer.php');?>
